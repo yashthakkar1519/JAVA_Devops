fd8270abb983138bf5c4862b1b0ff31f49099156
yash <yasht8888@gmail.com>
2023-02-20 11:32:30 +0530

    First_Commit

---
+++ JavaCode1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ReverseNumber {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter a number to reverse: ");
+    try {
+      int num = scanner.nextInt();
+      int reverse = 0;
+      while (num != 0) {
+        int digit = num % 10;
+        reverse = reverse * 10 + digit;
+        num /= 10;
+      }
+      System.out.println("Reverse of the number is: " + reverse);
+    } catch (Exception e) {
+      System.out.println("Error: " + e.getMessage());
+    }
+  }
+}

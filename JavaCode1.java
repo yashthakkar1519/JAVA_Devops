bc45166a6a5b51e3685ce189d0515e46313edc18
yashthakkar1519 <62497363+yashthakkar1519@users.noreply.github.com>
2023-02-21 15:17:38 +0530

    Update JavaCode1.java

--- JavaCode1.java
+++ JavaCode1.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
-# comment
-public class ReverseNumber {
+
+public class JavaCode1 {
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("Enter a number to reverse: ");
-    try {
-      int num = scanner.nextInt();
-      int reverse = 0;
-      while (num != 0) {
-        int digit = num % 10;
-        reverse = reverse * 10 + digit;
-        num /= 10;
-      }
-      System.out.println("Reverse of the number is: " + reverse);
-    } catch (Exception e) {
-      System.out.println("Error: " + e.getMessage());
-    }
+//     Scanner scanner = new Scanner(System.in);
+//     System.out.print("Enter a number to reverse: ");
+//     try {
+//       int num = scanner.nextInt();
+//       int reverse = 0;
+//       while (num != 0) {
+//         int digit = num % 10;
+//         reverse = reverse * 10 + digit;
+//         num /= 10;
+//       }
+//       System.out.println("Reverse of the number is: " + reverse);
+//     } catch (Exception e) {
+//       System.out.println("Error: " + e.getMessage());
+//     }
+    
+    System.out.println("Hi!");
   }
 }
